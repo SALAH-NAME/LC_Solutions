9064707f9cffe9957a45c0149cdab6a1ec0e12e1
SALAH-NAME <ouahidi.se@proton.me>
2025-05-22 05:58:25 +0100

    67_add-binary

---
+++ Easy/0067_add-binary/add-binary.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <string>
+
+class Solution
+{
+public:
+	std::string addBinary(std::string a, std::string b)
+	{
+		std::string res;
+		int i = a.size() - 1;
+		int j = b.size() - 1;
+		int carry = 0;
+		
+		while (i >= 0 || j >= 0 || carry)
+		{
+			if (i >= 0) carry += a[i--] - '0';
+			if (j >= 0) carry += b[j--] - '0';
+
+			res += carry % 2 + '0';
+			carry /= 2;
+		}
+		std::reverse(res.begin(), res.end());
+		return res;
+	}
+};
