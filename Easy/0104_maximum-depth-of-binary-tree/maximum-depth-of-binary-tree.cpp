ed1b5a976f360e2648dfaa297dc56aa1ee22f321
SALAH-NAME <ouahidi.se@proton.me>
2025-05-30 06:09:53 +0100

    104_maximum-depth-of-binary-tree

---
+++ Easy/0104_maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,26 @@
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	int maxDepth(TreeNode* root)
+	{
+		if (!root)
+			return 0;
+
+		int res = 1;
+		int left = maxDepth(root->left);
+		int right = maxDepth(root->right);
+		res += (left > right) ? left : right;
+		return res;
+	}
+};
