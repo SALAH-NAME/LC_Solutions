a35b008b39d6a521bc117cf0016e86cf5e0b6b93
Salah <Salah>
2025-06-04 19:14:15 +0100

    118_pascals-triangle

---
+++ Easy/0118_pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+class Solution
+{
+public:
+	std::vector<std::vector<int>> generate(int numRows)
+	{
+		std::vector<std::vector<int>> triangle(numRows);
+
+		triangle[0] = {1};
+		for (int i = 1; i < numRows; ++i)
+		{
+			std::vector<int> row(i + 1);
+			row[0] = 1, row[i] = 1;
+			for (int j = 1; j < i; ++j)
+				row[j] = triangle[i-1][j] + triangle[i-1][j - 1];
+			triangle[i] = row;
+		}
+		return triangle;
+	}
+};
