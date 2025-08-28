c5b5399349b7af88f0749d4a4d4886903aee4b8d
SALAH-NAME <ouahidi.se@proton.me>
2025-08-28 08:58:39 +0100

    563_binary-tree-tilt

---
+++ Easy/0563_binary-tree-tilt/binary-tree-tilt.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	int totalTilt = 0;
+	int dfs(TreeNode* root)
+	{
+		if (!root) return 0;
+		
+		int leftSum = dfs(root->left);
+		int rightSum = dfs(root->right);
+
+		totalTilt += std::abs(leftSum - rightSum);
+		return leftSum + rightSum + root->val;
+	}
+
+	int findTilt(TreeNode* root)
+	{
+		dfs(root);
+		return totalTilt;
+	}
+};
