2f69cc65f2dee50aab129c82323bbe82803989ec
SALAH-NAME <ouahidi.se@proton.me>
2025-09-13 06:53:13 +0100

    657_robot-return-to-origin

---
+++ Easy/0657_robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+class Solution
+{
+public:
+	bool judgeCircle(std::string moves)
+	{
+		int x = 0, y = 0;
+		for (char c : moves)
+		{
+			if (c == 'R') x++;
+			else if (c == 'L') x--;
+			else if (c == 'U') y++;
+			else if (c == 'D') y--;
+		}
+		return (x == 0 && y == 0);
+	}
+};
