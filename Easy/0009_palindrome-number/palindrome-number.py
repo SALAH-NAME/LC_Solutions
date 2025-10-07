ef57f953c97d860c7bd25cae6154907c22a32ef8
SALAH-NAME <ouahidi.se@proton.me>
2025-10-07 10:43:17 +0100

    9_palindrome-number.py

---
+++ Easy/0009_palindrome-number/palindrome-number.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = str(x)
+        l, r = 0, len(num) - 1
+        while l < r:
+            if (num[l] != num[r]):
+                return False
+            l += 1
+            r -= 1
+        return True
