8d05e59f0c1559a6c13367dbb2425100d78abf5b
SALAH-NAME <ouahidi.se@proton.me>
2025-11-15 08:37:42 +0100

    965_univalued-binary-tree.cpp

---
+++ Easy/0965_univalued-binary-tree/univalued-binary-tree.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+private:
+	bool dfs(TreeNode* root, int val)
+	{
+		if (!root) return true;
+		if (root->val != val) return false;
+		return dfs(root->left, val) && dfs(root->right, val);
+	}
+
+public:
+	bool isUnivalTree(TreeNode* root)
+	{
+		if (!root) return true;
+		return dfs(root, root->val);
+	}
+};
+
+std::vector<std::string> split(const std::string& s, char delimiter)
+{
+	std::vector<std::string> tokens;
+	std::string token;
+	std::stringstream ss(s);
+	while (std::getline(ss, token, delimiter))
+		tokens.push_back(token);
+	return tokens;
+}
+
+TreeNode* buildTree(const std::string& data)
+{
+	if (data.empty())
+		return nullptr;
+
+	std::vector<std::string> values = split(data, ',');
+	if (values.empty() || values[0] == "null") return nullptr;
+
+	TreeNode* root = new TreeNode(std::stoi(values[0]));
+	std::queue<TreeNode*> q;
+	q.push(root);
+	int i = 1;
+
+	while (!q.empty() && i < values.size())
+	{
+		TreeNode* node = q.front();
+		q.pop();
+
+		if (i < values.size() && values[i] != "null")
+		{
+			node->left = new TreeNode(std::stoi(values[i]));
+			q.push(node->left);
+		}
+		i++;
+		if (i < values.size() && values[i] != "null")
+		{
+			node->right = new TreeNode(std::stoi(values[i]));
+			q.push(node->right);
+		}
+		i++;
+	}
+	return root;
+}
+
+int main()
+{
+	std::vector<std::string> tests = {
+		"1,1,1,1,1,null,1",
+		"2,2,2,5,2",
+	};
+
+	for (auto& t : tests)
+	{
+		std::cout << "---\ntree: [" << t << "]\n";
+		TreeNode* tree = buildTree(t);
+		std::cout << (Solution().isUnivalTree(tree) ? "True" : "False");
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
