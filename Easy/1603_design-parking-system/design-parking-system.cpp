acdb9490c3e3c6af4f552e057b79a0c71e19b2b2
SALAH-NAME <ouahidi.se@proton.me>
2026-03-03 22:20:00 +0000

    1603_design-parking-system.cpp

---
+++ Easy/1603_design-parking-system/design-parking-system.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+class ParkingSystem
+{
+private:
+	int bigSlots;
+	int mediumSlots;
+	int smallSlots;
+
+public:
+	ParkingSystem(int big, int medium, int small) : bigSlots(big), mediumSlots(medium), smallSlots(small) {}
+
+	bool addCar(int carType)
+	{
+		if (carType == 1)
+		{
+			if (bigSlots > 0)
+			{
+				bigSlots--;
+				return true;
+			}
+		}
+		else if (carType == 2)
+		{
+			if (mediumSlots > 0)
+			{
+				mediumSlots--;
+				return true;
+			}
+		}
+		else if (carType == 3)
+		{
+			if (smallSlots > 0)
+			{
+				smallSlots--;
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+
+int main() {
+	ParkingSystem* obj = new ParkingSystem(1, 1, 0);
+	std::cout << obj->addCar(1) << std::endl; // true
+	std::cout << obj->addCar(2) << std::endl; // true
+	std::cout << obj->addCar(3) << std::endl; // false
+	std::cout << obj->addCar(1) << std::endl; // false
+	return 0;
+}
