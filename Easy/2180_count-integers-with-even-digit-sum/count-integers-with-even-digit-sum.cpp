d60e7081ec3d50a6c6c456e7f915bb50e9964939
SALAH-NAME <ouahidi.se@proton.me>
2026-06-23 05:58:20 +0100

    2180_count-integers-with-even-digit-sum.cpp

---
+++ Easy/2180_count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <iostream>
+
+class Solution
+{
+public:
+  int countEven(int num)
+  {
+    int count = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+      if (getDigitSum(i) % 2 == 0)
+      {
+        count++;
+      }
+    }
+    return count;
+  }
+private:
+  int getDigitSum(int n)
+  {
+    int sum = 0;
+    while (n > 0)
+    {
+      sum += n % 10;
+      n /= 10;
+    }
+    return sum;
+  }
+};
+
+typedef struct testCase
+{
+  int n;
+} testCase;
+
+int main()
+{
+  std::vector<testCase> tests = {
+    {4},
+    {30},
+  };
+
+  for (auto& [n] : tests)
+  {
+    std::cout << "---\nnum: " << n << "\ncountEven: ";
+    std::cout << Solution().countEven(n) << std::endl;
+  }
+  return 0;
+}
